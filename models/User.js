de9494e4eff912b89b2b664cb3be70e963923854
Korean2022 <108470232+Korean2022@users.noreply.github.com>
2025-03-27 21:09:56 +0900

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,18 +1,16 @@
 // models/User.js
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+
 const userSchema = new mongoose.Schema({
   username: { type: String, unique: true },
   password: String,
-  role: { type: String, default: 'user' } // admin 또는 user
+  role: { type: String, default: 'user' }
 });
 
-userSchema.pre('save', async function(next) {
+userSchema.pre('save', async function (next) {
   this.password = await bcrypt.hash(this.password, 10);
   next();
-
 });
 
 module.exports = mongoose.model('User', userSchema);
